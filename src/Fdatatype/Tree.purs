90307c67fb709ee21d7f1a3a2e52ad1f7749deb9
Tao Xiang <niemalsaufgeben@yeah.net>
2023-05-21 16:34:06 +0200

    1

--- src/Fdatatype/Tree.purs
+++ src/Fdatatype/Tree.purs
@@ -1,25 +1,24 @@
 module Fdatatype.Tree
   ( Tree(..)
   , bst
+  , buildBST
+  , inorder
+  , insertElement
+  , preorder
   , setTree
   )
   where
 
 import Prelude
-import Fdatatype.Set (Set, empty, insert, union,all)
+
+import Data.Array (concat)
+import Data.List (List(..))
+import Fdatatype.Set (Set, empty, insert, union, all)
 
 data Tree a
   = Leaf 
   | Node (Tree a) a (Tree a)
 
--- listTree :: forall a. Tree a -> List a
--- listTree tree = 
---   case tree of
---     Leaf -> Nil
---     Node left value right ->
---       let leftList = listTree left
---           rightList = listTree right
---       in concat List(leftList, singleton value, rightList)
 
 setTree :: forall a. Ord a => Tree a -> Set a
 setTree tree = 
@@ -29,6 +28,14 @@ setTree tree =
       let set1 = insert (setTree left) value
       in union set1 (setTree right)
 
+preorder :: forall a. Ord a => Tree a -> Array a
+preorder Leaf = []
+preorder (Node l a r) = concat [[a], preorder l,  preorder r]
+
+inorder :: forall a. Ord a => Tree a -> Array a
+inorder Leaf = []
+inorder (Node l a r) = concat [preorder l, [a], preorder r]
+
 bst :: forall a. Ord a => Tree a -> Boolean
 bst tree =
   case tree of
@@ -37,4 +44,17 @@ bst tree =
       let lt x = x <= value
           gt x = x >= value
       in all lt (setTree left) && all gt (setTree right) && bst left && bst right
+
+
+insertElement:: forall a. Ord a => a -> Tree a -> Tree a
+insertElement x Leaf = Node Leaf x Leaf
+insertElement x (Node l a r) 
+  | x < a = Node (insertElement x l) a r
+  | otherwise = Node l a (insertElement x r)
+
+
+buildBST :: forall a. Ord a => List a -> Tree a
+buildBST Nil = Leaf
+buildBST (Cons x xs) = insertElement x (buildBST xs)
+
 
--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,19 @@
 module Main where
 
+import Data.List
 import Prelude
 
+import Data.Array (toUnfoldable)
 import Effect (Effect)
 import Effect.Console (logShow)
-import Fdatatype.LTree (LTree(..), inorder)
--- import Fdatatype.Set (Set, empty)
--- import Fdatatype.Tree (Tree(..), bst)
+import Fdatatype.Tree (Tree(..), bst, buildBST, preorder, setTree)
+import Fdatatype.Tree (inorder)
+
+myArray :: Array Int
+myArray = [5,6,1,345,8]
+
+myList :: List Int
+myList = toUnfoldable myArray
 
 main :: Effect Unit
 main = do
@@ -16,6 +23,12 @@ main = do
   -- let tr = Node Leaf 5 (Node Leaf 3 Leaf)
   -- logShow (bst tr)
 
-  let tr = (Node (Leaf 3) (Leaf 5)) :: LTree Int
-  logShow(inorder tr)
+  -- let tr = (Node (Leaf 3) (Leaf 5)) :: LTree Int
+  -- logShow(inorder tr)
+
+  let niubi = buildBST myList
+  logShow(myList)
+  logShow(bst niubi)
+  logShow(preorder niubi)
+  logShow(inorder niubi)
   
