9528b3a690e475b741a3ddb037af720e3f5ac005
Tao Xiang <xiang.tao@outlook.de>
2023-06-03 21:31:08 +0200

    Update Sort.purs

--- src/Algos/Sort.purs
+++ src/Algos/Sort.purs
@@ -12,10 +12,10 @@ import Data.Tuple (Tuple(..))
 findMin :: forall a. Ord a => Partial => List a -> Tuple a (List a)
 findMin (x : Nil) = Tuple x Nil
 findMin (x : xs) =  let Tuple y ys = findMin xs 
-    in if x < y then Tuple x xs else Tuple y (Cons x ys)
+    in if x < y then Tuple x xs else Tuple y (x : ys)
 
 
 sel_sort :: forall a. Ord a =>Partial => List a -> List a 
 sel_sort Nil = Nil
-sel_sort xs = let Tuple m ms = findMin xs in (Cons m (sel_sort ms)) 
+sel_sort xs = let Tuple m ms = findMin xs in (m : (sel_sort ms)) 
 
